9b6968eb4b5eab53e05b46ff1fd196a737094bba
trajashekar423 <trajashekar423@gmail.com>
2024-05-16 18:57:44 +0530

    key

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -10,8 +10,8 @@ function About() {
         <div className="container">
             <div className="row">
             {Aboutitems.map(Aboutitems => (
-                <div className="col-4 mb-4" key={Aboutitems.id}>
-                    <div class="card">                   
+                <div className="col-4 mb-4">
+                    <div key={Aboutitems.id} class="card">                   
                             <div class="card-body">
                                 <h5 class="card-title">{Aboutitems.title}</h5>
                                 <p class="card-text">{Aboutitems.description}</p>
